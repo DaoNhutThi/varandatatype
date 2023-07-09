ae66a5fe26cc407bd5797bc541f4f43f66677a6f
DaoNhutThi <114686399+DaoNhutThi@users.noreply.github.com>
2023-07-09 12:30:30 +0700

    VariableAndDataType

---
+++ VariableAndDataType/Program.cs
@@ -0,0 +1,24 @@
+﻿//Console.WriteLine("Nhap ho va ten: ");
+//string input = Console.ReadLine();
+//Console.WriteLine("Nhap tuoi cua ban: ");
+//int length = int.Parse(Console.ReadLine());
+//Console.WriteLine("Ho va Ten: " + input);
+//Console.WriteLine("Tuoi: " + input1);
+//Console.WriteLine("Xin chao: " + input + " " + input1);
+
+//Console.WriteLine("Enter Length= ");
+//int length = int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter Width= ");
+//int width = int.Parse(Console.ReadLine());
+//int result = length* width;
+//Console.WriteLine("Area= "+ result);
+
+
+double r;
+double P = Math.PI;
+Console.WriteLine("enter  = ");
+r = Convert.ToDouble(Console.ReadLine());
+P = P * r * r;
+Console.WriteLine("Match.PI: " + P); ;
+
+
